3d4b56f73bdfc5e403d0532cc2823bf57a898393
federicogarcik <federico82008@gmail.com>
2017-04-20 12:08:43 -0300

    Add files via upload

--- Programacion y Laboratorio I/Practica/main.c
+++ Programacion y Laboratorio I/Practica/main.c
@@ -6,6 +6,7 @@ int main()
     char buffer[2000];
     char nombre[20];
     char apellido[20];
+    char apeNombre[20];
 
     printf("ingrese su nombre: ");
     gets(buffer);
@@ -25,9 +26,9 @@ int main()
         gets(buffer);
     }
     strcpy(apellido, buffer);
-    strcpy(buffer, nombre);
-    strcat(buffer, " ");
-    strcat(buffer, apellido);
+    strcpy(apeNombre, nombre);
+    strcat(apeNombre, " ");
+    strcat(apeNombre, apellido);
     printf("%s", buffer);
     return 0;
 }
